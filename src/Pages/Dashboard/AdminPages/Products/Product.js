6584d1f4da6a5b6c63acf7a406cd210205c849cc
Sultan Mahmud Ekram <smahmud1098@gmail.com>
2022-08-01 02:43:39 +0600

    delete tool functon added

--- src/Pages/Dashboard/AdminPages/Products/Product.js
+++ src/Pages/Dashboard/AdminPages/Products/Product.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import swal from 'sweetalert';
+import { setTools } from '../../../../Redux/Actions/toolsActions';
+import httpLink from '../../../../ServerLink/serverLink';
 
 const Product = ({ tool }) => {
-    const { img, name, price, sold } = tool;
+    const {_id, img, name, price, sold } = tool;
+    const dispatch = useDispatch();
+    
+    const handleDelete=()=>{
+        fetch(`${httpLink}/tool-delete`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ _id})
+        }).then(res => res.json()).then(() => {
+            fetch(`${httpLink}/tools`).then(res => res.json()).then(data =>{
+                swal("Deleted!", "Tool has been deleted.", "success");
+                dispatch(setTools(data))
+                }).catch(err => console.log(err))
+        }).catch(err => console.log(err))
+
+}
+
+
     return (
         <tr className='border my-2'>
             <td className="px-6 py-4">
@@ -19,7 +42,7 @@ const Product = ({ tool }) => {
                 {sold} Unit
             </td>
             <td className="px-6 py-4 ">
-                <button className='btn btn-outline bg-red'>Delete</button>
+                <button onClick={()=>handleDelete()} className='btn btn-outline bg-red'>Delete</button>
             </td>
 
         </tr>
